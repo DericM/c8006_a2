78ef921b6c487de2ba26134300ece8b10dd077fa
root <root@localhost.localdomain>
2018-02-10 17:39:49 -0800

    fixing ifconfig_client.sh

--- client_scripts/ifconfig_client.sh
+++ client_scripts/ifconfig_client.sh
@@ -1,20 +1,45 @@
-LAB_INTERFACE_NAME=eno1
-LAB_INTERFACE_IP=$(ifconfig $LAB_INTERFACE_NAME | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+#!/bin/bash
 
+#
+# This function initializes the variables used in the script.
+# LAB_* variables represent the external interface and ip associated with the outside network
+# LOCAL_* variables are internal interfaces and ips.
+#
+
+LAB_INTERFACE_NAME=eno1
 LOCAL_INTERFACE_NAME=enp3s2
 LOCAL_INTERFACE_IP=192.168.10.2
-
 LOCAL_INTERFACE_GATEWAY_IP=192.168.10.1
 
-echo "Flushing rules"
-#ip route del default
-#ip addr flush dev $LOCAL_INTERFACE_NAME
 
-echo "Deactivate $LAB_INTERFACE_NAME"
-ifconfig $LAB_INTERFACE_NAME down
+indent() { sed 's/^/  /'; }
 
-echo "Enable the second NIC that is connected to the firewall on $LOCAL_INTERFACE_IP"
-ifconfig p3p1 $LOCAL_INTERFACE_IP up
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-echo "Add a routing rule to route the firewall host as the default gateway for $LOCAL_INTERFACE_GATEWAY_IP"
-route add default gw $LOCAL_INTERFACE_GATEWAY_IP
+function init_client() {
+    echo Modifying ${bold}/etc/resolv.conf${normal}
+    sudo echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    echo "Flushing Routing Rules"
+    #ip route del default
+    #ip addr flush dev $LOCAL_INTERFACE_NAME
+    echo "Deactivating Lab Interface: ${bold}$LAB_INTERFACE_NAME ${normal}"
+    ifconfig $LAB_INTERFACE_NAME down
+
+    echo "Enabling Local Interface: ${bold}$LOCAL_INTERFACE_IP${normal}"
+    ifconfig $LOCAL_INTERFACE_NAME $LOCAL_INTERFACE_IP up
+
+    echo "Adding Routing Rule for: ${bold}$LOCAL_INTERFACE_GATEWAY_IP${normal}"
+    route add default gw $LOCAL_INTERFACE_GATEWAY_IP
+}
+
+function test_client() {
+    echo Testing Client
+}
+
+if [ $1 = 'init' ]; then
+    echo Initializing The Client
+    init_client | indent
+else
+    echo no valid argument passed
+fi
